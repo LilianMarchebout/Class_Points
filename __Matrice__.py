f9fcf38bb1eb1cb515da2583874bce9d1dc33af5
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-21 12:21:38 +0100

    Update __Matrice__.py

--- __Matrice__.py
+++ __Matrice__.py
@@ -15,20 +15,42 @@ class Matrice:
             l = []
             for i in range(len(self.line[key])):
                 l.append(self.line[key][i] + other.line[key][i])
-                d["line"+ str(i+1)] = l
+            d[str(key)] = l
         chaine = "Addition de " + self.name + " et de " + other.name + ": \n"
+        for key in d.keys():
+            chaine += str(d[key]) + "\n"
+        return chaine
+    def __sub__(self, other):
+        d = {}
+        for key in self.line.keys():
+            l = []
+            for i in range(len(self.line[key])):
+                l.append(self.line[key][i] - other.line[key][i])
+            d[str(key)] = l
+        chaine = "Soustraction de " + self.name + " et de " + other.name + ": \n"
+        for key in d.keys():
+            chaine += str(d[key]) + "\n"
+        return chaine
+    def __mul__(self, other):
+        d = {}
+        for i in range(len(self.line.keys())):
+            l = []
+            i = self.line["line" + str(i+1)]
+
+
+        d[str(i)] = l
+        chaine = "Multiplication de " + self.name + " et de " + other.name + ": \n"
         for key in d.keys():
             chaine += str(d[key]) + "\n"
         return chaine
 
 if __name__ == "__main__":
     print("Lancement du module __Point__ en cours...")
-    m = Matrice("m", line1=[56,54], line2=[51,0])
-    n = Matrice("n", line1=[45,0], line2=[0,55])
+    m = Matrice("m", line1=[1,4,4], line2=[6,8,4])
+    n = Matrice("n", line1=[2,1,3], line2=[4,1,5])
     print(m)
     print(n)
-    add = m + n
-    print(add)
+    print(m + n)
     print("Fin du module.")
     while 1:
         os.system("pause")
