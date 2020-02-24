a0c942c7f9e1a305156710c8386f452699f7961d
Lilian <35975697+Nalil10@users.noreply.github.com>
2020-02-24 12:13:24 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -10,11 +10,16 @@ class Fonction:
         self.fonction = fonction
     def __str__(self):
         return self.name + "(x) = "  + self.fonction
+    def y(self,x):
+        if x < 0:
+            x = "(" + str(x) + ")"
+        return eval(self.fonction.replace("x", str(x)))
 
 if __name__ == "__main__":
     print("Lancement du module __Fonction__ en cours...")
     f = Fonction("math.cos(x)")
     print(f)
+    print(f.y(90))
     print("Fin du module.")
     while 1:
         os.system("pause")
