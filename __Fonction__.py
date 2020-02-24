f1e4c19f7165b55753931ee2ad55406cced96f23
Lilian <35975697+Nalil10@users.noreply.github.com>
2020-02-24 12:06:35 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -5,5 +5,13 @@ import os
 import math
 
 class Fonction:
-    def __init__(self):
-        pass
+    def __init__(self, fonction):
+        self.fonction = fonction
+    def __str__(self):
+        return self.fonction
+
+if __name__ == "__main__":
+    print("Lancement du module __Fonction__ en cours...")
+    print("Fin du module.")
+    while 1:
+        os.system("pause")
