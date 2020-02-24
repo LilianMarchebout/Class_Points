32a1ce90836ccc308e8d5fa48e604485ce14f2ed
Lilian <35975697+Nalil10@users.noreply.github.com>
2020-02-24 12:10:11 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -5,13 +5,16 @@ import os
 import math
 
 class Fonction:
-    def __init__(self, fonction):
+    def __init__(self, fonction, name="f"):
+        self.name = name
         self.fonction = fonction
     def __str__(self):
-        return self.fonction
+        return self.name + "(x) = "  + self.fonction
 
 if __name__ == "__main__":
     print("Lancement du module __Fonction__ en cours...")
+    f = Fonction("math.cos(x)")
+    print(f)
     print("Fin du module.")
     while 1:
         os.system("pause")
